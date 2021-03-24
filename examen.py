a0b376b3fe460db491e328ca22aa434047cf1ed8
AdrianVaello <adrianvaello@gmail.com>
2021-03-24 11:22:02 +0100

    Checkpoint 3

--- examen.py
+++ examen.py
@@ -1,3 +1,6 @@
+from libro import Libro
+from autor import Autor
+
 def get_list(self, fichero):
     lista_palabras=[]
     lista_lon_palabras=[]
@@ -5,11 +8,8 @@ def get_list(self, fichero):
     for i in range(20):
         dic_palabras[i]=" "
     with open(fichero, mode="rt", encoding="utf-8") as f:
-        print("dentro de f" +str(f.read(10)))
-        texto_vacio=False
         for linea in f:
             aux=linea.split(" ")
-            print("aux"+aux)
             for p in aux:
                 repetida= linea.count(p)
                 lista_lon_palabras.append(len(p))
@@ -22,6 +22,21 @@ def get_list(self, fichero):
             if pa == len(j):
                 lista_aux.append(j)
             dic_palabras[pa]= lista_aux
-    #print(dic_palabras)
+    print(dic_palabras)
 
 get_list(self="ra", fichero="frase.txt")
+
+#-------------------------------------
+l1= Libro(autor=Autor(id_autor="1",nombre="Adrian", apellido="Vaello"),titulo="Las leyendas de pepe",anyo=2021)
+l2= Libro(autor=Autor(id_autor="2",nombre="Nacho", apellido="kajs"),titulo="Las historias de aroon",anyo=2014)
+l3= Libro(autor=Autor(id_autor="3",nombre="Stephen", apellido="Vaello"),titulo="Harry potter",anyo=1978)
+lista_libros=[]
+lista_libros.append(l1)
+lista_libros.append(l2)
+lista_libros.append(l3)
+
+def mas_antiguos(self, libros, ano):
+    for l in libros:
+        
+
+mas_antiguos(self="sf",libros=lista_libros,ano=2000)
