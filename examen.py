205a305b7e8c3be0469d56adb05f2878553cfe10
AdrianVaello <adrianvaello@gmail.com>
2021-03-24 11:03:20 +0100

    Checkpoint 2

--- examen.py
+++ examen.py
@@ -2,16 +2,19 @@ def get_list(self, fichero):
     lista_palabras=[]
     lista_lon_palabras=[]
     dic_palabras={}
-    con=0
     for i in range(20):
         dic_palabras[i]=" "
     with open(fichero, mode="rt", encoding="utf-8") as f:
+        print("dentro de f" +str(f.read(10)))
+        texto_vacio=False
         for linea in f:
             aux=linea.split(" ")
-            #print(aux)
+            print("aux"+aux)
             for p in aux:
+                repetida= linea.count(p)
                 lista_lon_palabras.append(len(p))
                 lista_palabras.append(p)
+
     
     for pa in lista_lon_palabras:
         lista_aux=[]
@@ -19,7 +22,6 @@ def get_list(self, fichero):
             if pa == len(j):
                 lista_aux.append(j)
             dic_palabras[pa]= lista_aux
-        con+=1
-    print(dic_palabras)
+    #print(dic_palabras)
 
 get_list(self="ra", fichero="frase.txt")
